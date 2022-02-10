0505b30ca2295e5f69800f184ff9aeb0f52b44ca
unknown <levi.millikin@gmail.com>
2022-02-10 14:00:00 +0100

    new kata

---
+++ String repeat.js
@@ -0,0 +1,16 @@
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+
+
+function repeatStr (n, s) {
+    let string = "";
+    while (n > 0){
+      string += s;
+      n--;
+    }
+    return string;
+  }
