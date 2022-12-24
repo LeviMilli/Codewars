be721c97151659a39ae006cf1a3b41f28cc51809
unknown <levi.millikin@gmail.com>
2022-12-24 04:25:19 -0800

    new kata

---
+++ Unique-in-order.js
@@ -0,0 +1,24 @@
+
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+// For example:
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1
+
+
+
+var uniqueInOrder=function(iterable){
+  
+    let strArr = Array.isArray(iterable) ? iterable : iterable.split('');
+    
+    let answer = []
+    for (let i = 0; i < strArr.length; i++){
+      if (strArr[i] !== strArr[i+1] || strArr[i] == undefined){
+        answer.push(strArr[i])
+      }
+    } return answer
+    //your code here - remember iterable can be a string or an array
+    console.log(iterable)
+  }
