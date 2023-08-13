34de1da1dc2380b332db360a144dc994d564c9a7
Levi Milli <levi.millikin@gmail.com>
2023-08-12 21:23:16 -0600

    javascript kata

---
+++ ReturnNegative.js
@@ -0,0 +1,6 @@
+function returnNegative(num) {
+    return num > 0 ? num - (num + num) :  num
+}
+
+// we take number and check if its greater than 0, we return the value that is double negative of itself.
+// else we return the number
